12aa7f6e0b89a2b6d77b22b0f99172a7a1692993
ilyamishkin <iluhamishkin@gmail.com>
2019-04-04 21:40:46 +0200

    Created block part for image gallery

---
+++ template-parts/block-parts/part-mini-images.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * 
+ * This is the block gallery of images
+ * 
+ */
+$images = $block['mini_images']; ?>
+
+<div class="block d-flex jcc fwrap pt32 pb32">
+    <?php foreach ( $images as $image ): 
+        $img = wp_get_attachment_image($image['mini_images_image'], 'full', false, array('class' => 'block-image__el block-image__el--contain')); 
+        $size = $image['mini_images_size']; ?>
+    <div class="mbl-col-8 tbl-col-<?Php echo $size; ?>">
+        <div class="block-image block-image--256">
+            <?php echo $img; ?>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</div>
