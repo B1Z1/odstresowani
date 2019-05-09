031c896a89f4d8870e369f10f23dcf8e8e7a504a
ilyamishkin <iluhamishkin@gmail.com>
2019-05-09 17:10:46 +0200

    Layout Animation head is created

--- template-parts/animation-block/anime-head.php
+++ template-parts/animation-block/anime-head.php
@@ -7,11 +7,11 @@
 
 ?>
 
-<div class="animation animation-head">
-    <img src="<?php echo get_template_directory_uri() ?>/assets/img/animation/brain.svg" alt="Stress Head" class="animation-head__image">
-    <div class="animation__line animation-head__linesmall"></div>
-    <div class="animation__line animation-head__linebig"></div>
-    <div class="animation__rect animation-head__rect--first"></div>
-    <div class="animation__rect animation-head__rect--second"></div>
-    <div class="animation__rect animation-head__rect--third"></div>
+<div class="l-head">
+    <div class="l-head__rect"></div>
+    <div class="l-head__rect"></div>
+    <div class="l-head__rect"></div>
+    <div class="l-head__linesmall"></div>
+    <div class="l-head__linebig"></div>
+    <img src="<?php echo get_template_directory_uri() ?>/assets/img/animation/brain.svg" alt="Stress Head" class="l-head__image">
 </div>
