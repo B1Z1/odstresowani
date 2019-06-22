78e6b81732ef2eba45547440cc8edae766818756
ilyamishkin <iluhamishkin@gmail.com>
2019-06-22 16:26:46 +0200

    Changed content most viewed

--- includes/fields/fields-block-most-viewed.php
+++ includes/fields/fields-block-most-viewed.php
@@ -9,18 +9,20 @@ use Carbon_Fields\Block;
  * Im using everywhere
  *
  */
-add_action( 'carbon_fields_register_fields', 'crb_register_block_most_viewed', 10 );
-if ( !function_exists('crb_register_block_most_viewed') ){
-    function crb_register_block_most_viewed(){
+add_action('carbon_fields_register_fields', 'crb_register_block_most_viewed', 10);
+if (!function_exists('crb_register_block_most_viewed')) {
+    function crb_register_block_most_viewed()
+    {
         $posts = GetPosts::getPostsTypes();
 
         Block::make(__('Blok z najbardziej oglądającymi postami'))
+            ->add_fields(FieldsGlobal::getGaps())
             ->add_fields(array(
                 Field::make('select', 'most_viewed_posts', __('Wybierz typ postów'))
                     ->set_options($posts),
             ))
             ->set_render_callback(function ($block) {
-                include(locate_template('template-parts/content-most-viewed-3.php',false, false) );
+                include(locate_template('template-parts/content-most-viewed-3.php', false, false));
             });
     }
 }
--- template-parts/content-most-viewed-3.php
+++ template-parts/content-most-viewed-3.php
@@ -2,7 +2,7 @@
 wp_reset_postdata();
 global $currlang;
 
-$post = $block['most_viewed_posts'] ? $block['most_viewed_posts']:'any';
+$post = $block['most_viewed_posts'] ? $block['most_viewed_posts'] : 'any';
 
 //Arguments for Query
 $args_q = array(
@@ -14,38 +14,40 @@ $args_q = array(
     'lang' => $currlang,
 );
 $posts = new WP_Query($args_q);
+$gapsClasses = GeneralOptions::getAllGapsFromArray($block);
+
 ?>
 
-<?php if ( $block['most_viewed_posts'] ): ?>
-</div>
+<?php if ($block['most_viewed_posts']) : ?>
+    </div>
 <?php endif; ?>
 
-    <div class="most-viewed block mt128 mb128">
-        <h3 class="reset-top block--texcen mb64"><?php pll_e('Najchętniej czytane'); ?></h3>
-        <div class="d-flex fwrap row jcc">
-            <?php if ( $posts->have_posts() ): ?>
-                <?php while ( $posts->have_posts() ): $posts->the_post(); ?>
+<div class="most-viewed block <?php echo $gapsClasses; ?>">
+    <h3 class="reset-top block--texcen mb64"><?php pll_e('Najchętniej czytane'); ?></h3>
+    <div class="d-flex fwrap row jcc">
+        <?php if ($posts->have_posts()) : ?>
+            <?php while ($posts->have_posts()) : $posts->the_post(); ?>
 
-                        <div class="ntb-col-6 pc-col-4">
-                            <?php
-                            /**
-                             *
-                             * Post type action
-                             * -> Mini card
-                             *
-                             */
-                            $args['image'] = get_the_post_thumbnail_url(get_the_ID(), 'full');
-                            $args['title'] = get_the_title();
-                            $args['url'] = get_permalink();
-                            do_action('post_card_mini', $args);
-                            ?>
-                        </div>
+                <div class="ntb-col-6 pc-col-4">
+                    <?php
+                    /**
+                     *
+                     * Post type action
+                     * -> Mini card
+                     *
+                     */
+                    $args['image'] = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                    $args['title'] = get_the_title();
+                    $args['url'] = get_permalink();
+                    do_action('post_card_mini', $args);
+                    ?>
+                </div>
 
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
+</div>
 
-<?php if ( $block['most_viewed_posts'] ): ?>
-<div class="c-container">
+<?php if ($block['most_viewed_posts']) : ?>
+    <div class="c-container">
 <?php endif; ?>
