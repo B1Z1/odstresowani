982ed16258aa3a8f81ec54b22e75d4fd26e6ad68
ilyamishkin <iluhamishkin@gmail.com>
2019-06-22 16:30:01 +0200

    Changed banner

--- includes/fields/fields-block-post-banner.php
+++ includes/fields/fields-block-post-banner.php
@@ -8,25 +8,27 @@ use Carbon_Fields\Block;
  * Block Banner for posts
  *
  */
-add_action( 'carbon_fields_register_fields', 'crb_register_block_post_banner', 10 );
-if ( !function_exists('crb_register_block_post_banner') ){
-    function crb_register_block_post_banner(){
+add_action('carbon_fields_register_fields', 'crb_register_block_post_banner', 10);
+if (!function_exists('crb_register_block_post_banner')) {
+    function crb_register_block_post_banner()
+    {
         Block::make(__('Blok banner dla postów'))
+            ->add_fields(FieldsGlobal::getGaps())
             ->add_fields(array(
                 Field::make('image', 'banner_image', __('Zdjęcie dla banera')),
                 Field::make('text', 'banner_autor', __('Podaj autora zdjęcia')),
                 Field::make('select', 'banner_size', __('Podaj rozmiar obrazu'))
-                    ->set_options( array(
+                    ->set_options(array(
                         'small' => 'Mały',
                         'full' => 'Duży',
                         '2xfull' => 'Podwójnie Duży'
                     ))
                     ->set_default_value('small'),
                 Field::make('checkbox', 'banner_type', __('Czy to jest gif?'))
-                    ->set_option_value( 'Tak' )
+                    ->set_option_value('Tak')
             ))
             ->set_render_callback(function ($block) {
-                include(locate_template('template-parts/block-parts/part-banner.php',false, false) );
+                include(locate_template('template-parts/block-parts/part-banner.php', false, false));
             });
     }
 }
--- template-parts/block-parts/part-banner.php
+++ template-parts/block-parts/part-banner.php
@@ -4,10 +4,11 @@ $size = $block['banner_size'] ? $block['banner_size']:'full';
 $image = wp_get_attachment_image_src($block['banner_image'], 'full')[0];
 $icon = wp_get_attachment_image_src( carbon_get_theme_option('general_sygnet_mini'), 'full' )[0];
 $type = $block['banner_type'];
+$gapsClasses = GeneralOptions::getAllGapsFromArray($block);
 ?>
 <?php if ( $image ): ?>
     </div>
-        <div class="l-pubimage mt64 mb64">
+        <div class="l-pubimage <?php echo $gapsClasses; ?>">
             <div class="l-pubimage__container l-pubimage--<?php echo $size ?>">
                 <img src="<?php echo $image; ?>" class="c-image <?php if ( $type ){ echo 'c-image--contain'; } ?>" alt="Sygnet">
                 <div class="l-pubimage__sygnet bck-wh flex-kit jcc">
