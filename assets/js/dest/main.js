f42d9f84c6646825970cd8258c4d0129ae1fcb3e
ilyamishkin <iluhamishkin@gmail.com>
2019-06-23 02:42:29 +0200

    ADded model

--- assets/js/dest/main.js
+++ assets/js/dest/main.js
@@ -21,12 +21,12 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   function n(i) {
     if (t[i]) return t[i].exports;
-    var o = t[i] = {
+    var r = t[i] = {
       i: i,
       l: !1,
       exports: {}
     };
-    return e[i].call(o.exports, o, o.exports, n), o.l = !0, o.exports;
+    return e[i].call(r.exports, r, r.exports, n), r.l = !0, r.exports;
   }
 
   n.m = e, n.c = t, n.d = function (e, t, i) {
@@ -47,10 +47,10 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     if (n.r(i), Object.defineProperty(i, "default", {
       enumerable: !0,
       value: e
-    }), 2 & t && "string" != typeof e) for (var o in e) {
-      n.d(i, o, function (t) {
+    }), 2 & t && "string" != typeof e) for (var r in e) {
+      n.d(i, r, function (t) {
         return e[t];
-      }.bind(null, o));
+      }.bind(null, r));
     }
     return i;
   }, n.n = function (e) {
@@ -115,7 +115,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
           t && function (t) {
             var n = _toConsumableArray(document.querySelectorAll(".header__triger")),
                 i = 0 - (window.innerHeight - t.offsetHeight - 50),
-                o = new IntersectionObserver(function (n) {
+                r = new IntersectionObserver(function (n) {
               n.forEach(function (n) {
                 var i = n.isIntersecting;
                 i ? e.addClass("l-header--active", t) : e.removeClass("l-header--active", t);
@@ -126,7 +126,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
             });
 
             n.forEach(function (e) {
-              o.observe(e);
+              r.observe(e);
             });
           }(t);
         }
@@ -138,8 +138,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
           !function (t) {
             var n = document.querySelector(".".concat(t.slider_block)),
                 i = _toConsumableArray(document.querySelectorAll(".".concat(t.slider_cards))),
-                o = document.querySelector(".".concat(t.slider_pagination)),
-                r = {
+                r = document.querySelector(".".concat(t.slider_pagination)),
+                o = {
               pag_active: t.classes.pag_active,
               card_active: t.classes.card_active,
               pagination_el: t.classes.pagination_el,
@@ -149,33 +149,33 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
             if (n) {
               i.forEach(function (i, a) {
-                var l = i,
-                    c = l.dataset.title,
-                    d = l.dataset.name,
+                var c = i,
+                    l = c.dataset.title,
+                    d = c.dataset.name,
                     u = "";
-                e.removeClass(r.card_active, l), s && (u = l.querySelector(".".concat(t.slider_has_back)).dataset.back, s = !0), 0 == a && (e.addClass(r.card_active, l), s && (n.style.backgroundImage = "url(".concat(u, ")"))), function (t, n, i, o, r, s) {
+                e.removeClass(o.card_active, c), s && (u = c.querySelector(".".concat(t.slider_has_back)).dataset.back, s = !0), 0 == a && (e.addClass(o.card_active, c), s && (n.style.backgroundImage = "url(".concat(u, ")"))), function (t, n, i, r, o, s) {
                   var a = document.createElement("li"),
-                      l = "";
-                  e.addClass(r.pagination_el, a), a.className += " ".concat(r.extra), 0 == o && e.addClass(r.pag_active, a), a.dataset.name = n, "" != s ? (l = document.createElement(s.wrapper), "" != s.classes && (l.className += s.classes), l.textContent = i, a.appendChild(l)) : a.textContent = i;
+                      c = "";
+                  e.addClass(o.pagination_el, a), a.className += " ".concat(o.extra), 0 == r && e.addClass(o.pag_active, a), a.dataset.name = n, "" != s ? (c = document.createElement(s.wrapper), "" != s.classes && (c.className += s.classes), c.textContent = i, a.appendChild(c)) : a.textContent = i;
                   t.appendChild(a);
-                }(o, d, c, a, r, t.pagination_wrap);
+                }(r, d, l, a, o, t.pagination_wrap);
               });
 
-              var a = _toConsumableArray(o.children);
+              var a = _toConsumableArray(r.children);
 
-              a.forEach(function (o) {
-                var l = o,
-                    c = l.dataset.name,
+              a.forEach(function (r) {
+                var c = r,
+                    l = c.dataset.name,
                     d = i.find(function (e) {
-                  if (e.dataset.name == c) return e;
+                  if (e.dataset.name == l) return e;
                 }),
                     u = "";
-                s && (u = d.querySelector(".".concat(t.slider_has_back)).dataset.back), l.addEventListener("click", function () {
+                s && (u = d.querySelector(".".concat(t.slider_has_back)).dataset.back), c.addEventListener("click", function () {
                   i.map(function (t) {
-                    e.removeClass(r.card_active, t);
+                    e.removeClass(o.card_active, t);
                   }), a.map(function (t) {
-                    e.removeClass(r.pag_active, t);
-                  }), e.addClass(r.card_active, d), e.addClass(r.pag_active, l), s && (n.style.backgroundImage = "url(".concat(u, ")"));
+                    e.removeClass(o.pag_active, t);
+                  }), e.addClass(o.card_active, d), e.addClass(o.pag_active, c), s && (n.style.backgroundImage = "url(".concat(u, ")"));
                 });
               });
             }
@@ -190,12 +190,12 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
             var t = document.querySelector(e.container),
                 n = t ? _toConsumableArray(t.children) : null;
             var i = [];
-            n && (t.innerHTML = "", o(), window.addEventListener("scroll", function (e) {
+            n && (t.innerHTML = "", r(), window.addEventListener("scroll", function (e) {
               var i = 0 - (t.getBoundingClientRect().top - this.innerHeight);
-              i > t.offsetHeight && n.length > 0 && o();
+              i > t.offsetHeight && n.length > 0 && r();
             }));
 
-            function o() {
+            function r() {
               i = n.slice(0, 9), n.splice(0, 9), i.forEach(function (e) {
                 t.appendChild(e);
               });
@@ -261,9 +261,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
             var t = e.querySelector(".".concat(_this.video, " video")),
                 n = t.dataset.src,
                 i = e.querySelector(".".concat(_this.controls.container)),
-                o = i.querySelector(".".concat(_this.controls.play)),
-                r = i.querySelector(".".concat(_this.controls.stop.el));
-            _this.installCurrentTime(t), t.volume = _this.currentVolume, _this.onPlay(o, t, n, r, e), _this.onStop(t, r, e);
+                r = i.querySelector(".".concat(_this.controls.play)),
+                o = i.querySelector(".".concat(_this.controls.stop.el));
+            _this.installCurrentTime(t), t.volume = _this.currentVolume, _this.onPlay(r, t, n, o, e), _this.onStop(t, o, e);
           });
         }
       }, {
@@ -279,14 +279,14 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
         }
       }, {
         key: "onPlay",
-        value: function onPlay(e, t, n, i, o) {
+        value: function onPlay(e, t, n, i, r) {
           var _this3 = this;
 
           e.addEventListener("click", function () {
-            void 0 !== _this3.last && (_this3.last.querySelector("video").pause(), _this3.installCurrentTime(_this3.last.querySelector("video")), _this3.last.classList.remove(_this3.activeElement), _this3.last.querySelector(".".concat(_this3.controls.stop.el)).classList.remove(_this3.controls.stop.active)), _this3.installCurrentTime(t), o.classList.add(_this3.activeElement), _this3.last = o, t.querySelector('source[type="video/mp4"]').src = n, setTimeout(function () {
+            void 0 !== _this3.last && (_this3.last.querySelector("video").pause(), _this3.installCurrentTime(_this3.last.querySelector("video")), _this3.last.classList.remove(_this3.activeElement), _this3.last.querySelector(".".concat(_this3.controls.stop.el)).classList.remove(_this3.controls.stop.active)), _this3.installCurrentTime(t), r.classList.add(_this3.activeElement), _this3.last = r, t.querySelector('source[type="video/mp4"]').src = n, setTimeout(function () {
               window.scrollTo({
-                top: o.getBoundingClientRect().top + window.scrollY
-              }), t.load(), t.play(), i.classList.add(_this3.controls.stop.active), o.querySelector(".filter-back").classList.add("fade");
+                top: r.getBoundingClientRect().top + window.scrollY
+              }), t.load(), t.play(), i.classList.add(_this3.controls.stop.active), r.querySelector(".filter-back").classList.add("fade");
             }, 500);
           });
         }
@@ -319,8 +319,17 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
       function _class2() {
         _classCallCheck(this, _class2);
 
-        this.scene = new THREE.Scene(), console.log(this.scene);
+        this.scene = new THREE.Scene(), this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1e3), this.renderer = new THREE.WebGLRenderer(), this.container = {
+          element: document.querySelector("[data-dna-three]")
+        }, this.container.width = this.container.element.offsetWidth, this.container.height = this.container.element.offsetHeight, this.init();
       }
+
+      _createClass(_class2, [{
+        key: "init",
+        value: function init() {
+          this.renderer.setSize(this.container.width, this.container.height), this.container.element.appendChild(this.renderer.domElement);
+        }
+      }]);
 
       return _class2;
     }())();
