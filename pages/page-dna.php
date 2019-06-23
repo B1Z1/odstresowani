b5fee63090af5c2d709749fcad6445e15ece91e2
ilyamishkin <iluhamishkin@gmail.com>
2019-06-23 16:02:05 +0200

    Changed location of three files

--- pages/page-dna.php
+++ pages/page-dna.php
@@ -154,8 +154,8 @@ get_header();
         <!-- ------------------ -->
         <!--  Main section end  -->
         <!-- ------------------ -->
-        <script src="<?php echo get_template_directory_uri() . '/assets/js/three.min.js' ?>"></script>
-        <script src="<?php echo get_template_directory_uri() . '/assets/js/inflate.min.js' ?>"></script>
+        <script src="<?php echo get_template_directory_uri() . '/assets/lin/THREEFBX/three.min.js' ?>"></script>
+        <script src="<?php echo get_template_directory_uri() . '/assets/lin/THREEFBX/inflate.min.js' ?>"></script>
         <script src="<?php echo get_template_directory_uri() . '/assets/js/dest/brain.js' ?>"></script>
 <?php
 get_footer();
