050153990e9a272aac212ab33bd2b91e45e6c693
ilyamishkin <iluhamishkin@gmail.com>
2019-06-29 14:56:52 +0200

    Created module

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="text" class="searchform__input" placeholder="Wyszukaj..." value="<?php echo get_search_query(); ?>" name="s" required>
-    <button type="submit" class="searchform__button" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>"><i class="fas fa-search"></i></button>
+<form role="search" method="get" class="m-SearchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <input type="text" class="m-SearchForm__input" placeholder="Wyszukaj..." value="<?php echo get_search_query(); ?>" name="s" required>
+    <button type="submit" class="m-SearchForm__button" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>"><i class="fas fa-search"></i></button>
 </form>
